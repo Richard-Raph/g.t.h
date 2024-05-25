1684c6a5759ce18036c2bab7566d58e8905f85fd
itsjustonyii <onyinyechiokeke733@gmail.com>
2024-05-25 19:14:52 +0100

    Created the SideBar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
+import NavBar from "./Components/NavBar/NavBar";
+import Analytics from "./Pages/Analytics/Analytics";
 
 function App() {
 
   return (
     <>
-    
+    <NavBar />
+    <Routes>
+    <Route path='/analytics' element={ <Analytics /> } />
+    </Routes>
     </>
   )
 }
